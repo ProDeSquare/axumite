c44c45831b28328cd8839229c95ef5ec72a402d0
Hamza Mughal <hamza@prodesquare.com>
2025-11-06 20:53:19 +0500

    chore: added example validation route

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,1 +1,2 @@
 pub mod health_model;
+pub mod validation_model;
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,10 +5,12 @@ use crate::{
 use axum::{Router, routing::get};
 
 mod health_routes;
+mod validation_routes;
 
 pub fn create_router() -> Router<AppState> {
     Router::new()
         .route("/", get(root_handler))
         .merge(health_routes::router())
+        .merge(validation_routes::router())
         .fallback(not_found_handler)
 }
